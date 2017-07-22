119e8aa91e8d62722245bd80ee6e1213e1165453
kamal11k <kamal.panigrahi@mountblue.io>
2017-07-22 10:20:57 +0530

    Feat: changed directory structure. Created language heading

--- app/index.js
+++ app/index.js
@@ -1,16 +1,7 @@
 var React = require('react');
 var ReactDom = require('react-dom')
 require('./index.css')
-
-class App extends React.Component {
-    render(){
-        return(
-            <div>
-                Hello Kamal!!
-            </div>
-        )
-    }
-}
+var App = require('./components/App');
 
 ReactDom.render(
     <App />,

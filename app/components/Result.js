e2083bc168cf6b3d22926cf8dfd8bbd6aae18fb9
kamal11k <kamal.panigrahi@mountblue.io>
2017-07-26 10:44:14 +0530

    Feat: used querystring to fetch player details

--- app/components/Result.js
+++ app/components/Result.js
@@ -1,6 +1,17 @@
 import React from 'react'
+import queryString from 'query-string'
+import {battle} from '../utils/api'
+
 
 class Result extends React.Component {
+    componentDidMount(){
+        var players = queryString.parse(this.props.location.search);
+
+        battle([players.playerOneName,players.playerTwoName])
+            .then((results)=>{
+                console.log(results)
+            })
+    }
     render(){
         return (
             <div>Result page</div>
